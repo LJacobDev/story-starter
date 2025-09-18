cb143e4f32d36f5d39a3e2f7b67827735651e3bf
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-09-18 02:15:27 -0400

    sign in / sign up form fixed, doesn't cling to bottom of window

--- src/components/AuthContainer.vue
+++ src/components/AuthContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-background p-4">
+  <div class="flex items-center justify-center p-0 w-full">
     <div class="w-full max-w-md">
       <Transition 
         name="auth-form" 
